216fa7a6dbcda551773a851fc57e57e0f2e82ecf
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2022-11-12 19:26:40 -0500

    turtle that makes underground room

---
+++ roomMakerTurtle/mine5x3.lua
@@ -0,0 +1,41 @@
+function fivexone()
+
+	turtle.dig()
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.turnRight()
+	turtle.dig()
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.turnRight()
+	turtle.dig()
+
+	turtle.turnRight()
+	turtle.forward()
+	turtle.forward()
+	turtle.turnLeft()
+
+
+	turtle.turnRight()
+	turtle.forward()
+	turtle.turnLeft()
+	turtle.dig()
+	turtle.turnRight()
+	turtle.forward()
+	turtle.turnLeft()
+	turtle.dig()
+
+	turtle.turnLeft()
+	turtle.forward()
+	turtle.forward()
+	turtle.turnRight()
+
+end
+
+fivexone()
+turtle.up()
+fivexone()
+turtle.up()
+fivexone()
+turtle.down()
+turtle.down()
