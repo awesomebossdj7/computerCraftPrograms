95d32a45fd05cebef564c915faa79ed1c50ab4e2
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2022-11-23 16:53:02 -0500

    AweOSv1.1

---
+++ AweOS/cmdExec.lua
@@ -0,0 +1,42 @@
+paintutils.drawFilledBox(6,4,46,17,colors.black)
+paintutils.drawLine(6,4,45,4,colors.gray)
+paintutils.drawPixel(46,4,colors.red)
+term.setCursorPos(46,4)
+term.write("X")
+
+term.setBackgroundColor(colors.gray)
+term.setCursorPos(6,4)
+term.write("Command Execute")
+
+term.setBackgroundColor(colors.black)
+
+c = peripheral.find("command")
+
+term.setCursorPos(6,5)
+term.write("type in command:")
+term.setCursorPos(6,6)
+cmd = read() 
+
+c.setCommand(cmd)
+output = c.runCommand()
+c.setCommand("")
+
+term.setCursorPos(6,7)
+if output == false then
+    print("Invalid Command")
+    sleep(1)
+    paintutils.drawFilledBox(6,4,46,17,colors.cyan)
+    return
+end
+
+while true do
+event, button, x, y = os.pullEvent("mouse_click")
+term.setCursorPos(6,5)
+term.setBackgroundColor(colors.black)
+
+if x == 46 and y == 4 then
+    paintutils.drawFilledBox(1,1,51,20,colors.cyan)
+return
+end
+sleep(0.1)
+end
