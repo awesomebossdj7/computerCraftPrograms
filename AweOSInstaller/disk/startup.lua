170f8d52613fe2420a7165ee6f5fc0c9c64b1ab0
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2022-11-23 16:57:31 -0500

    Update startup.lua

--- AweOSInstaller/disk/startup.lua
+++ AweOSInstaller/disk/startup.lua
@@ -1,5 +1,5 @@
 shell.run("pastebin get wPtGKMam github")
-shell.run("github OwOsucksAwesomeMinecraftbossAlissha6TRM computerCraftPrograms . AweOS")
-shell.run("move downloads/computerCraftPrograms/AweOS AweOS")
+shell.run("github OwOsucksAwesomeMinecraftbossAlissha6TRM computerCraftPrograms . AweOS/AweOS")
+shell.run("move downloads/computerCraftPrograms/AweOS/AweOS AweOS")
 shell.run("github OwOsucksAwesomeMinecraftbossAlissha6TRM computerCraftPrograms . AweOSInstaller/")
 shell.run("move downloads/computerCraftPrograms/AweOSInstaller/startup.lua startup")
