276cd4b53cd498035f528aee65d6f2b1dc4b0714
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-03-09 00:54:51 -0500

    Update door.lua
    moved the num pad to the left so it looks better

--- plethora/switchcraft3/door.lua
+++ plethora/switchcraft3/door.lua
@@ -12,34 +12,34 @@ m.setTextScale(1)
 
 
 --top row
-m.setCursorPos(2,1)
+m.setCursorPos(1,1)
 m.write("1")
 
-m.setCursorPos(4,1)
+m.setCursorPos(3,1)
 m.write("2")
 
-m.setCursorPos(6,1)
+m.setCursorPos(5,1)
 m.write("3")
 
 --middle row
-m.setCursorPos(2,3)
+m.setCursorPos(1,3)
 m.write("4")
 
-m.setCursorPos(4,3)
+m.setCursorPos(3,3)
 m.write("5")
 
-m.setCursorPos(6,3)
+m.setCursorPos(5,3)
 m.write("6")
 
 --bottom row
 
-m.setCursorPos(2,5)
+m.setCursorPos(1,5)
 m.write("7")
 
-m.setCursorPos(4,5)
+m.setCursorPos(3,5)
 m.write("8")
 
-m.setCursorPos(6,5)
+m.setCursorPos(5,5)
 m.write("9")
 
 --zero
@@ -57,23 +57,23 @@ input = ""
 while true do
 	event, side, x, y = os.pullEvent("monitor_touch")
 	if side == "monitor_640" then
-		if x == 2 and y == 1 then
+		if x == 1 and y == 1 then
 			input = input.."1"
-		elseif x == 4 and y == 1 then
+		elseif x == 3 and y == 1 then
 			input = input.."2"
-		elseif x == 6 and y == 1 then
+		elseif x == 5 and y == 1 then
 			input = input.."3"
-		elseif x == 2 and y == 3 then
+		elseif x == 1 and y == 3 then
 			input = input.."4"
-		elseif x == 4 and y == 3 then
+		elseif x == 3 and y == 3 then
 			input = input.."5"
-		elseif x == 6 and y == 3 then
+		elseif x == 5 and y == 3 then
 			input = input.."6"
-		elseif x == 2 and y == 5 then
+		elseif x == 1 and y == 5 then
 			input = input.."7"
-		elseif x == 4 and y == 5 then
+		elseif x == 3 and y == 5 then
 			input = input.."8"
-		elseif x == 6 and y == 5 then
+		elseif x == 5 and y == 5 then
 			input = input.."9"
 		elseif x == 7 and y == 3 then
 			input = input.."0"
