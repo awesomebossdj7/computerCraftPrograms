3eab899db29d8dd267e8b58941368cd03380e218
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-03-11 05:47:53 -0500

    Update startup.lua

--- AweOSInstaller/disk/startup.lua
+++ AweOSInstaller/disk/startup.lua
@@ -1,6 +1,11 @@
 shell.run("pastebin get wPtGKMam github")
 shell.run("github OwOsucksAwesomeMinecraftbossAlissha6TRM computerCraftPrograms . AweOS/AweOS")
 fs.move("downloads/computerCraftPrograms/AweOS/AweOS", "AweOS/")
+if fs.exists("startup.lua") then
+  fs.delete("startup.lua")
+elseif fs.exists("startup") then
+  fs.delete("startup")
+end
 file = fs.open("startup.lua","w")
 file.write("shell.run('AweOS/PasswordSys.lua')")
 file.close()
