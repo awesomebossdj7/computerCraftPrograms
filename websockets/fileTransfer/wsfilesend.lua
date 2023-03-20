b9365be5ec29cfc42e750aacac7ea25d7f01a686
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-03-19 21:37:44 -0400

    Create wsfilesend.lua

---
+++ websockets/fileTransfer/wsfilesend.lua
@@ -0,0 +1,14 @@
+ws = http.websocket("")
+ 
+args = {...}
+ 
+fileDir = args[1]
+ 
+ 
+file = fs.open(fileDir,"r")
+fileData = file.readAll()
+file.close()
+ 
+serialFileData = textutils.serialise(fileData)
+ 
+ws.send(serialFileData)
