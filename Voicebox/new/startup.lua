1b40a5cdedbe14837044fc4932c59b996c5aec32
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-06-05 09:46:39 -0400

    Update startup.lua

--- Voicebox/new/startup.lua
+++ Voicebox/new/startup.lua
@@ -3,21 +3,23 @@ while true do
 
 local message = msg
 
-  local url = "https://music.madefor.cc/tts?text=" .. textutils.urlEncode(message)
-  local response, err = http.get { url = url, binary = true }
-  if not response then error(err, 0) end
+  if user == "awesomehome7_dj" then
+    local url = "https://music.madefor.cc/tts?text=" .. textutils.urlEncode(message)
+    local response, err = http.get { url = url, binary = true }
+    if not response then error(err, 0) end
 
-  local speaker = peripheral.find("speaker")
-  local decoder = require("cc.audio.dfpwm").make_decoder()
+    local speaker = peripheral.find("speaker")
+    local decoder = require("cc.audio.dfpwm").make_decoder()
 
-  while true do
-      local chunk = response.read(16 * 1024)
-      if not chunk then break end
+    while true do
+        local chunk = response.read(16 * 1024)
+        if not chunk then break end
 
-      local buffer = decoder(chunk)
-      while not speaker.playAudio(buffer) do
-          os.pullEvent("speaker_audio_empty")
-      end
+        local buffer = decoder(chunk)
+        while not speaker.playAudio(buffer) do
+            os.pullEvent("speaker_audio_empty")
+        end
+    end
   end
   sleep(0.1)
 end
