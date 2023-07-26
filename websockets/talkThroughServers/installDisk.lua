fa2cae66c32f6e54f003fb7fd5be5825ae12fea3
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-07-26 18:50:54 -0400

    Create installDisk.lua

---
+++ websockets/talkThroughServers/installDisk.lua
@@ -0,0 +1,3 @@
+shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/talkThroughServers/startupDisk.lua disk/startup")
+shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/talkThroughServers/send.lua disk/send.lua")
+shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/talkThroughServers/receive.lua disk/receive.lua")
