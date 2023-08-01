96d5e56d5002fc3afa11a3581643357022808166
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-08-01 00:04:55 -0400

    Update install.lua

--- websockets/turtleControl/install.lua
+++ websockets/turtleControl/install.lua
@@ -1,1 +1,4 @@
-test
+shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/turtleControl/main.lua")
+shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/turtleControl/startup.lua")
+shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/turtleControl/update.lua")
+os.reboot()
