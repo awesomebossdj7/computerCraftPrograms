3415060d0e46ec1962bbd4accde589feafbcedba
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-08-01 18:01:06 -0400

    Update install.lua

--- websockets/talkThroughServers/install.lua
+++ websockets/talkThroughServers/install.lua
@@ -1,3 +1,4 @@
 shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/talkThroughServers/startup.lua")
 shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/talkThroughServers/send.lua")
 shell.run("wget https://raw.githubusercontent.com/OwOsucksAwesomeMinecraftbossAlissha6TRM/computerCraftPrograms/main/websockets/talkThroughServers/receive.lua")
+os.reboot()
