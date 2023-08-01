cef6de7b169a2f7ee0e5ebda6c9f7f387bb1002d
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-08-01 18:00:14 -0400

    Update receive.lua

--- websockets/talkThroughServers/receive.lua
+++ websockets/talkThroughServers/receive.lua
@@ -1,4 +1,4 @@
-ws2 = http.websocket("98.209.180.144:5001")
+ws2 = http.websocket("wss://ccws.skystuff.cc/connect/turtleempire-receive/turtlesrule") --5001
 
 while true do
     event, user, chatmsg = os.pullEvent("chat")
