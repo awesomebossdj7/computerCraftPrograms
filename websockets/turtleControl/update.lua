d7a885c325cffb775d91e8f9f77d856b0b50f025
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2023-08-01 17:02:55 -0400

    Update update.lua

--- websockets/turtleControl/update.lua
+++ websockets/turtleControl/update.lua
@@ -1,11 +1,16 @@
 ws = http.websocket("98.209.180.144:6969")
 
 while true do
-	msg = ws.receive()
-	if msg == nil then
-		sleep(0.1)
-	else
-		shell.run(msg)
+	msg = textutils.unserialiseJSON(ws.receive())
+	
+
+	if msg.id == os.getComputerId() then
+		if msg.cmd == nil then
+			sleep(0.1)
+		else
+			shell.run(msg)
+		end
 	end
+	
 	sleep(0.1)
 end
