00b80630ffe102466a5fde838da96a20b02f8ddc
DJ <70352423+awesomebossdj7@users.noreply.github.com>
2024-12-09 01:26:18 -0500

    Add files via upload

---
+++ websockets/turtleControl/new/startup/main.lua
@@ -0,0 +1,49 @@
+local oldPull = os.pullEvent
+os.pullEvent = os.pullEventRaw
+
+if os.getComputerLabel() == nil then
+    shell.run(".rom/rename.lua")
+end
+
+if _G.WS == nil then
+    shell.run(".rom/websocket.lua")
+end
+
+shell.run(".rom/printOverwrite.lua")
+
+function main()
+    while true do
+        fake = _G.WS.receive()
+        tbl = textutils.unserialiseJSON(fake)
+        if tbl["type"] ~= nil then
+            if tbl["type"] == "function" then
+                if tonumber(tbl["id"]) == os.getComputerID() then
+                    loadstring(tbl["msg"])()
+                end
+            end
+        end
+        sleep(0)
+    end
+end
+
+function events()
+    while true do
+        event, arg1, arg2, arg3 = os.pullEvent()
+        if event ~= "websocket_message" then
+            tbl = {}
+            tbl["event"] = event
+            tbl["arg1"] = arg1
+            tbl["arg2"] = arg2
+            tbl["arg3"] = arg3
+            tbl["type"] = "event"
+            tbl["id"] = os.getComputerID()
+            _G.WS2.send(textutils.serialiseJSON(tbl))
+        end
+        sleep(0)
+    end
+end
+
+parallel.waitForAny(main,events)
+--more code or somethin idfk i hate ni-
+
+os.pullEvent = oldPull
